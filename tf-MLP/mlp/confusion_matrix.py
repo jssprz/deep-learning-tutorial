ce17eaa77eeaf078dd66af8f5b3205a2e95f0d0b
Jesus Perez Martin <jperezmartin90@gmail.com>
2019-04-08 01:27:46 -0400

    adds the confusion_matrix.py file. Now the confusion-matrix image, the accuracy and recall are logged and they can be displayed using tensorboard

---
+++ tf-MLP/mlp/confusion_matrix.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+"""Provides the plot_confusion_matrix.
+plot_confusion_matrix generates the confusion matrix from a lists of truths and predicted labels
+"""
+
+from textwrap import wrap
+import re
+import itertools
+import tfplot
+import numpy as np
+import matplotlib
+from sklearn.metrics import confusion_matrix
+
+__author__ = "jssprz"
+__version__ = "0.0.1"
+__maintainer__ = "jssprz"
+__email__ = "jperezmartin90@gmail.com"
+__status__ = "Development"
+
+
+def plot_confusion_matrix(correct_labels, predict_labels, labels, tensor_name='MyFigure/image', normalize=False):
+    """
+    :param predict_labels: These are you predicted classification categories
+    :param correct_labels: These are your true classification categories.
+    :param labels: This is a lit of labels which will be used to display the axis labels
+    :param tensor_name: Name for the output summary tensor
+    :param normalize: Set this to True if you want to normalize the values of the confusion matrix
+
+    Returns:
+        summary: TensorFlow summary
+
+    Other items to note:
+        - Depending on the number of category and the data, you may have to modify the figzie, font sizes etc.
+        - Currently, some of the ticks don't line up due to rotations.
+    """
+
+    cm = confusion_matrix(correct_labels, predict_labels, labels=labels)
+    if normalize:
+        cm = cm.astype('float') * 10 / cm.sum(axis=1)[:, np.newaxis]
+        cm = np.nan_to_num(cm, copy=True)
+        cm = cm.astype('int')
+
+    np.set_printoptions(precision=2)
+
+    fig = matplotlib.figure.Figure(figsize=(7, 7), dpi=320, facecolor='w', edgecolor='k')
+    ax = fig.add_subplot(1, 1, 1)
+    ax.imshow(cm, cmap='Oranges')
+
+    classes = [re.sub(r'([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))', r'\1 ', x) for x in labels]
+    classes = ['\n'.join(wrap(l, 40)) for l in classes]
+
+    tick_marks = np.arange(len(classes))
+
+    ax.set_xlabel('Predicted', fontsize=7)
+    ax.set_xticks(tick_marks)
+    ax.set_xticklabels(classes, fontsize=4, rotation=-90, ha='center')
+    ax.xaxis.set_label_position('bottom')
+    ax.xaxis.tick_bottom()
+
+    ax.set_ylabel('True Label', fontsize=7)
+    ax.set_yticks(tick_marks)
+    ax.set_yticklabels(classes, fontsize=4, va='center')
+    ax.yaxis.set_label_position('left')
+    ax.yaxis.tick_left()
+
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        ax.text(j, i, format(cm[i, j], 'd') if cm[i, j] != 0 else '.',
+                horizontalalignment='center', fontsize=6, verticalalignment='center', color='black')
+    fig.set_tight_layout(True)
+
+    return tfplot.figure.to_summary(fig, tag=tensor_name)
--- tf-MLP/mlp/mlp_model.py
+++ tf-MLP/mlp/mlp_model.py
@@ -6,85 +6,109 @@ Created on Mar 4, 2019
 
 import tensorflow as tf
 
-def gaussian_weights(shape,  mean, stddev):
-    return tf.truncated_normal(shape, 
-                               mean = mean, 
-                               stddev = stddev)
+
+def gaussian_weights(shape, mean, stddev):
+    return tf.truncated_normal(shape,
+                               mean=mean,
+                               stddev=stddev)
 
 
-def fc_layer(_input, size, name, use_sigmoid=True):
+def fc_layer(_input, size, name, activation='sigmoid', activation_param=0.1):
     """
     a fully connected layer
-    """         
+    """
+
     # shape is a 1D tensor with 4 values
     num_features_in = _input.get_shape().as_list()[-1]
-    #reshape to  1D vector    
-    shape = [num_features_in, size]
-    W = tf.Variable(gaussian_weights(shape, 0.0, 0.02), name=name)     
-    b = tf.Variable(tf.zeros(size))
-    #just a  multiplication between input[N_in x D]xW[N_in x N_out]
-    layer = tf.add( tf.matmul(_input, W) ,  b)        
-    if use_sigmoid:
-        layer=tf.nn.sigmoid(layer)
-    return  layer
 
-def mlp_fn(features,  input_size, n_classes):    
-    with tf.variable_scope("mlp_scope"):                                
-        # fc 1
+    # reshape to  1D vector
+    shape = [num_features_in, size]
+    W = tf.Variable(gaussian_weights(shape, 0.0, 0.02), name=name)
+    b = tf.Variable(tf.zeros(size))
+
+    # just a  multiplication between input[N_in x D]xW[N_in x N_out]
+    layer = tf.add(tf.matmul(_input, W), b)
+
+    if activation == 'sigmoid':
+        layer = tf.nn.sigmoid(layer)
+    if activation == 'tanh':
+        layer = tf.nn.tanh(layer)
+    if activation == 'relu':
+        layer = tf.nn.relu(layer)
+    if activation == 'leaky-relu':
+        layer = tf.nn.leaky_relu(layer, alpha=activation_param)
+
+    return layer
+
+
+def mlp_fn(features, input_size, n_classes, activation, activation_param):
+    with tf.variable_scope("mlp_scope"):
+        # input
         features = tf.reshape(features, [-1, input_size])
         print(" features {} ".format(features.get_shape().as_list()))
-        fc1 = fc_layer(features, 100, name = 'fc1')    
-        #fc5 = layers.dropout_layer(fc5, 0.8)
+
+        # fully connected layer 1
+        fc1 = fc_layer(features, 100, name='fc1', activation=activation, activation_param=activation_param)
         print(" fc1: {} ".format(fc1.get_shape().as_list()))
-        #fc 6
-        fc2 = fc_layer(fc1, 100, name = 'fc2')            
+
+        # fully connected layer 2
+        fc2 = fc_layer(fc1, 100, name='fc2', activation=activation, activation_param=activation_param)
         print(" fc2: {} ".format(fc2.get_shape().as_list()))
-        #fully connected
-        fc3 = fc_layer(fc2, n_classes, name = 'fc3', use_sigmoid = False )
+
+        # fully connected layer 3, output layer
+        fc3 = fc_layer(fc2, n_classes, name='fc3', activation='')
         print(" fc3: {} ".format(fc3.get_shape().as_list()))
-                        
+
     return {"output": fc3}
 
-#defining a model that feeds the Estimator
-def model_fn (features, labels, mode, params):
+
+# defining a model that feeds the Estimator
+def model_fn(features, labels, mode, params):
     """The signature here is standard according to Estimators. 
        The output is an EstimatorSpec
     """
-    
-    net = mlp_fn(features, params['input_size'], params['number_of_classes'])                    
-    #    
-    output = net["output"]
-                
-    predicted_probs = tf.nn.softmax(output, name="predicted_probs")
-    #--------------------------------------    
-    # If prediction mode, predictions is returned
-    predictions = { "predicted_probs": predicted_probs,                    
-                   }
+
+    net = mlp_fn(features,
+                 params['input_size'],
+                 params['number_of_classes'],
+                 params['activation'],
+                 params['activation-param'])
+    logits = net["output"]
+
+    idx_predicted_class = tf.argmax(logits, 1)
+
     if mode == tf.estimator.ModeKeys.PREDICT:
-        estim_specs = tf.estimator.EstimatorSpec(mode, predictions=predictions)
-    else : # TRAIN or EVAL        
+        # --------------------------------------
+        # If prediction mode, predictions is returned
+        predictions = {
+            'class_ids': idx_predicted_class[:, tf.newaxis],
+            'probabilities': tf.nn.softmax(logits),
+            'logits': logits,
+        }
+        return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+    else:  # TRAIN or EVAL
         idx_true_class = tf.argmax(labels, 1)
-        idx_predicted_class = tf.argmax(output,1)            
-        # Evaluate the accuracy of the model
-        acc_op = tf.metrics.accuracy(labels=idx_true_class, predictions=idx_predicted_class)    
+
+        # Define the evaluation metrics of the model
+        acc_op, accuracy_update = tf.metrics.accuracy(labels=idx_true_class, predictions=idx_predicted_class)
+        recall_op = tf.metrics.recall(labels=idx_true_class, predictions=idx_predicted_class)
+        false_neg_op = tf.metrics.false_negatives(labels=idx_true_class, predictions=idx_predicted_class)
+
         # Define loss - e.g. cross_entropy - mean(cross_entropy x batch)
-        cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits = output, labels = labels)                 
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=labels)
         loss = tf.reduce_mean(cross_entropy)
-        optimizer = tf.train.AdamOptimizer(learning_rate = params['learning_rate'])
-        train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())        
-        #EstimatorSpec 
-        estim_specs = tf.estimator.EstimatorSpec(
+
+        if params['optimizer'] == 'gradient-descent':
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate=params['learning_rate'])
+        else:
+            optimizer = tf.train.AdamOptimizer(learning_rate=params['learning_rate'])
+
+        train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+
+        # EstimatorSpec
+        return tf.estimator.EstimatorSpec(
             mode=mode,
             predictions=idx_predicted_class,
             loss=loss,
             train_op=train_op,
-            eval_metric_ops={'accuracy': acc_op})    
-    
-    return  estim_specs
-
-
-
-
-
-
-
+            eval_metric_ops={'accuracy': (acc_op, accuracy_update), 'recall': recall_op, 'false_neg': false_neg_op})
