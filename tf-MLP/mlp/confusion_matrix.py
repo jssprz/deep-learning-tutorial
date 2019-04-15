3d44d33dfb7e725c906462f9a5a46a8a4f1ebed4
Jesus Perez Martin <jperezmartin90@gmail.com>
2019-04-15 12:29:52 -0400

    changes the confusion-matrix normalization

--- tf-MLP/mlp/confusion_matrix.py
+++ tf-MLP/mlp/confusion_matrix.py
@@ -36,9 +36,8 @@ def plot_confusion_matrix(correct_labels, predict_labels, labels, tensor_name='M
 
     cm = confusion_matrix(correct_labels, predict_labels, labels=labels)
     if normalize:
-        cm = cm.astype('float') * 10 / cm.sum(axis=1)[:, np.newaxis]
+        cm = cm.astype('float') * 100 / cm.sum(axis=1)[:, np.newaxis]
         cm = np.nan_to_num(cm, copy=True)
-        cm = cm.astype('int')
 
     np.set_printoptions(precision=2)
 
@@ -64,7 +63,7 @@ def plot_confusion_matrix(correct_labels, predict_labels, labels, tensor_name='M
     ax.yaxis.tick_left()
 
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        ax.text(j, i, format(cm[i, j], 'd') if cm[i, j] != 0 else '.',
+        ax.text(j, i, format(cm[i, j], '.2f' if normalize else 'd') if cm[i, j] != 0 else '.',
                 horizontalalignment='center', fontsize=6, verticalalignment='center', color='black')
     fig.set_tight_layout(True)
 
