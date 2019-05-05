f49dfdddb3521242c8fb601960fd9bb268e4a77a
Jesus Perez Martin <jperezmartin90@gmail.com>
2019-05-04 21:57:23 -0400

    changes the cnn architecture

--- ConvNet/cnnLib/cnn_arch.py
+++ ConvNet/cnnLib/cnn_arch.py
@@ -29,7 +29,7 @@ def mnistnet_fn(features, input_shape, n_classes, n_channels, is_training=True):
         pool_1 = layers.max_pool_layer(conv_1, 3, 2)  # 14x14
         print(" pool_1: {} ".format(pool_1.get_shape().as_list()))
 
-        conv_2 = layers.conv_layer(conv_1, shape=[3, 3, 32, 64], name='conv_2', is_training=is_training)
+        conv_2 = layers.conv_layer(pool_1, shape=[3, 3, 32, 64], name='conv_2', is_training=is_training)
         print(" conv_2: {} ".format(conv_2.get_shape().as_list()))
 
         pool_2 = layers.max_pool_layer(conv_2, 3, 2)  # 7x7
